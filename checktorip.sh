7cb454b56cc498ee99e77fe14265621ac66319f3
Jeff Hammett <jeff@jeffhammett.com>
2014-10-03 14:38:24 -0700

    simplified input

--- checktorip.sh
+++ checktorip.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# Takes input of IP, port and log filename. IP and port are those of the server that you would like to check Tor access to
+# Takes input of log filename
 
-IP=$1
-port=$2
-log=$3
-: ${port:="80"}
+log=$1
+
 : ${log:="access.log"}
 
-string="TorBulkExitList.py?ip=$IP&port=$port"
-
-if [ -f $string ]
+if [ -f exit-addresses ]
 then
-        rm $string
+        rm exit-addresses
 fi
 
-wget "https://check.torproject.org/cgi-bin/$string"
+wget https://check.torproject.org/exit-addresses
 
-while read string; do grep "$string" /var/log/apache2/$log; done < $string
+grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' exit-addresses | while read line; do grep "$line" /var/log/apache2/$log; done
