d36068da26c0893462eb009ea87e4f8e7355e921
Jeff Hammett <jeff@jeffhammett.com>
2014-10-03 12:21:55 -0700

    updated torexitcountry.sh to output to stdout and write to file

--- torexitcountry.sh
+++ torexitcountry.sh
@@ -5,7 +5,7 @@
 
 wget https://check.torproject.org/exit-addresses
 
-grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' exit-addresses | while read -r line; do geoiplookup "$line"; done | grep Country | grep -v V6 | grep -v "Address not found" | sort | uniq -c > countrylist.txt
+grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' exit-addresses | while read -r line; do geoiplookup "$line"; done | grep Country | grep -v V6 | grep -v "Address not found" | sort | uniq -c | tee torcountrylist.txt
 
 rm exit-addresses
 
